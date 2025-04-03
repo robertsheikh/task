7c4330bcf9669aefb3ef0d9d247907375db2ecb3
robertsheikh <robertsheikh07@gmail.com>
2025-04-03 17:20:09 +0300

    1

---
+++ 12video_pageLinks/views/about.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+
+<div class="min-h-full">
+ 
+<!-- navbar -->
+<?php require "partials/nav.php"; ?>
+
+  <header class="bg-white shadow-sm">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1>Hello i am in About Page</h1>
+    </div>
+  </main>
+</div>
+
+</body>
+</html>
---
+++ 12video_pageLinks/views/index.view.php
@@ -0,0 +1,20 @@
+<!-- Header -->
+ <?php require ("partials/head.php") ?>
+
+ <!-- navbar -->
+<?php require ("partials/nav.php"); ?>
+
+
+  <header class="bg-white shadow-sm">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1>Hello i am in Homepage</h1>
+    </div>
+  </main>
+
+<!-- Footer -->
+<?php require ("partials/footer.php"); ?>
